7212958a32be08ecfa6522de7aa996d3eecdbc10
Roman <123395035+Kukuru5a@users.noreply.github.com>
2023-05-11 21:54:20 +0600

    Add files via upload
    Fixed Cli.java

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Cli {
     public static void playerWelcome() {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Can I have your name? ");
+        System.out.print("May I have your name? ");
         var userName = sc.next();
         System.out.print("Hello, " + userName + "!");
         sc.close();
