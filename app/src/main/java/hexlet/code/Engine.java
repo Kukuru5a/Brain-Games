59a40b0591cbb929e03bbfc79ff843b449fcaa0e
Roman <123395035+Kukuru5a@users.noreply.github.com>
2023-05-11 22:06:02 +0600

    Add files via upload
    Fixed

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -31,6 +31,6 @@ public class Engine {
                 return;
             }
         }
-        System.out.println("Congratulation " + userName + "!");
+        System.out.println("Congratulations, " + userName + "!");
     }
 }
